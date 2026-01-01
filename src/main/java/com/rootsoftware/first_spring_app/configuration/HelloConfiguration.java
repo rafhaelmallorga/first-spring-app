53e8d8a63140584a032577f802fb58802c360be5
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2026-01-01 19:03:54 -0300

    new: first commit

---
+++ src/main/java/com/rootsoftware/first_spring_app/configuration/HelloConfiguration.java
@@ -0,0 +1,15 @@
+package com.rootsoftware.first_spring_app.configuration;
+
+// External class injection.
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+
+    @Bean
+    public MyService sdkAWS() {
+        return new MyServiceImpl();
+    }
+}
